ce7a736a13d1c5c72936f84ecf5e1eb870597e7f
mahbub <mahbub.mmhr@gmail.com>
2026-03-11 12:50:43 -0700

    move fault injector to parent directory

--- configs/fault_injector/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ $# -lt 1 ]]; then
-    echo "Usage: $0 <Source file>"
-    exit 1
-fi
-
-filename=$(basename "${1%.*}")
-dirname=$(dirname "$1")
-clang -o0 -static $1 -o ${dirname}/${filename}.bin
